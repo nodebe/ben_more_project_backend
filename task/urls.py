c8fb7bdce77653a566ceea89a48e1caee12c1222
Nodebe <nodywelete3@gmail.com>
2024-03-17 22:14:00 +0100

    update

--- task/urls.py
+++ task/urls.py
@@ -5,7 +5,7 @@ from task.views import TaskApiView, TaskOptionsApiView
 
 urlpatterns = [
     path('', TaskApiView.as_view()),
+    path('options', TaskOptionsApiView.as_view()),
     path('project/<str:pid>', TaskApiView.as_view()),
     path('<str:tid>', TaskApiView.as_view()),
-    path('options', TaskOptionsApiView.as_view())
 ]
